fd7fcb53620270e781bd57ed496beb8fe3d61aa1
Irina Serova <isserova@gmail.com>
2018-08-12 21:34:48 -0700

    rest parameters for average

--- average.js
+++ average.js
@@ -1,15 +1,20 @@
 /*
  * Programming Quiz: Using the Rest Parameter (1-5)
  */
-
-// your code goes here
+//
+//average(2, 6) should return 4
+//average(2, 3, 3, 5, 7, 10) should return 5
+//average(7, 1432, 12, 13, 100) should return 312.8
+//average() should return 0
 
 function average(...nums) {
-	let average = 0;
-	for (const num of nums){
-		average += num/nums.length;
-	}
-    return = average;
+    let total = 0;
+    for(const num of nums){
+        total += num;
+    }
+    result = total/nums.length;
+    return result;
+    
 }
 
 console.log(average(2, 6));
