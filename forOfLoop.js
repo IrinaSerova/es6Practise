bd6fcd81bda4594e985d379eb6139b09fcfc72ef
Irina Serova <isserova@gmail.com>
2018-08-12 19:54:07 -0700

    prints the day out to the console

--- forOfLoop.js
+++ forOfLoop.js
@@ -1,7 +1,9 @@
 /*
  * Programming Quiz: Writing a For...of Loop (1-4)
  */
-
+//loops through each day in the days array
+//capitalizes the first letter of the day
+//and prints the day out to the console
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 // your code goes here
