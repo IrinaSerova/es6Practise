138e7609725192efde3645ea9f37bb5c9d37995b
Irina Serova <isserova@gmail.com>
2018-08-12 19:51:08 -0700

    prints the day out to the console

--- forOfLoop.js
+++ forOfLoop.js
@@ -5,3 +5,7 @@
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 // your code goes here
+for (const day of days){
+	
+	console.log(`${day[0].toUpperCase()}${day.substr(1)}`);
+}
